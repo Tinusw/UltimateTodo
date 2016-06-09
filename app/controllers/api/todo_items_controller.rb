f73fa3dbb83927c947dcef25a7aa87fbb68d52b5
Tinus <vertelmy@gmail.com>
2016-06-09 13:18:41 +1200

    Impliment Create for API

--- app/controllers/api/todo_items_controller.rb
+++ app/controllers/api/todo_items_controller.rb
@@ -3,6 +3,11 @@ class Api::TodoItemsController < ApplicationController
     render json: todo_list.todo_items
   end
 
+  def create
+    todo_list.todo_items.create!(params.permit(:desc))
+    render nothing: true
+  end
+
   private
   def todo_list
     TodoList.find_by_id(params[:todo_list_id])
--- spec/controllers/api/todo_items_controller_spec.rb
+++ spec/controllers/api/todo_items_controller_spec.rb
@@ -2,24 +2,42 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe Api::TodoItemsController do 
-  describe "#index" do 
-    context "for user that has a todo list with some todos" do 
-      let(:todo_list) { create(:todo_list, :with_todo_items) }
-      let(:todo1) { todo_list.todo_items[0] }
-      let(:todo2) { todo_list.todo_items[1] }
-      let(:user) { todo_list.user }
+  context "for a new logged-in user" do
+    let(:user) { create(:user) }
+    before { sign_in(user) }
 
-      before do 
-        sign_in(user)
+
+    describe "#index" do 
+      context "for a todo list with some todos" do 
+        let(:todo_list) { create(:todo_list, :with_todo_items, user: user) }
+        let(:todo1) { todo_list.todo_items[0] }
+        let(:todo2) { todo_list.todo_items[1] }
+
+        it "should return json of those tasks" do 
+          get :index, todo_list_id: todo_list.id
+          todo_item = JSON.parse(response.body)
+          todo_item.should == [
+            {'id' => todo1.id, 'desc' => todo1.desc, 'priority' => nil, 'due_date' => nil, 'completed' => false},
+            {'id' => todo2.id, 'desc' => todo2.desc, 'priority' => nil, 'due_date' => nil, 'completed' => false}
+          ]
+        end
+      end
+    end
+
+    describe "#create" do 
+      let(:post_create) do 
+        post :create, todo_list_id: user.todo_list.id, desc: "new todo item"
       end
 
-      it "should return json of those tasks" do 
-        get :index, todo_list_id: todo_list.id
-        todo_item = JSON.parse(response.body)
-        todo_item.should == [
-          {'id' => todo1.id, 'desc' => todo1.desc, 'priority' => nil, 'due_date' => nil, 'completed' => false},
-          {'id' => todo2.id, 'desc' => todo2.desc, 'priority' => nil, 'due_date' => nil, 'completed' => false}
-        ]
+      it "should add the record to the db" do 
+        expect {
+          post_create
+        }.to change(TodoItem, :count).by(1)
+      end
+
+      it "should return 200 OK" do 
+        post_create
+        response.should be_success
       end
     end
   end
