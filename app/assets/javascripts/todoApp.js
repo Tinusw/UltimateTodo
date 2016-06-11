28592eeceb884b39edeb29c8f75cbaf4dbf930b6
Tinus <vertelmy@gmail.com>
2016-06-11 18:31:34 +1200

    BREAK ALL THE ANGULARS

--- app/assets/javascripts/todoApp.js
+++ app/assets/javascripts/todoApp.js
@@ -1,19 +1,31 @@
-var app = angular.module('todoApp', ['ngResource']);
+var app = angular.module('todoApp', []);
+
+// AngularJS & TurboLinks fix
+// $(document).on('ready page:load', function() {
+//     angular.bootstrap(document.body, ['todoApp']);
+// });
+
+// TodoList Controller
+app.controller('TodoListCtrl', ['$scope', function ($scope){
+  $scope.todos =[
+    {id: 1, title: "wash rug"},
+    {id: 2, title: "oil baby"},
+    {id: 3, title: "yoogoo"}
+  ];
+
+  $scope.addTodo = function(){
+    $scope.todos.push($scope.newTodo);
+  }
+}]);
+
+
 
 // csrf token config
 
-app.config(function($httpProvider) {
-  var authToken;
-  authToken = $("meta[name=\"csrf-token\"]").attr("content");
-  return $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
-});
+// app.config(function($httpProvider) {
+//   var authToken;
+//   authToken = $("meta[name=\"csrf-token\"]").attr("content");
+//   return $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
+// });
 
 
-// AngularJS & TurboLinks fix
-$(document).on('page:load', function() {
-  return $('[ng-app]').each(function() {
-    var module;
-    module = $(this).attr('ng-app');
-    return angular.bootstrap(this, [module]);
-  });
-});
